d12e0a2baddde8fc35d924f71517f5dabefb58c8
21ricardozamora <zamoraricardo21288@gmail.com>
2024-10-09 18:51:19 +0100

    se hace un ejercicio

--- ut2/cadenas/calc-basic/main.py
+++ ut2/cadenas/calc-basic/main.py
@@ -1,5 +1,14 @@
 def run():
-    # TODO
+    number1 = int(input('Enter a number: '))
+    number2 = int(input('Enter a number: '))
+    result_add = number1 + number2
+    result_substract = number1 - number2
+    result_mult = number1 * number2
+    result_div = number1 // number2
+    print(number1, '+', number2, '=', result_add, sep='')
+    print(number1, '-', number2, '=', result_substract, sep='')
+    print(number1, '*', number2, '=', result_mult, sep='')
+    print(number1, '/', number2, '=', result_div, sep='')
 
 
 # DO NOT TOUCH THE CODE BELOW
