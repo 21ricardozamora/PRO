fc75bcf63bba2ca89a6b3278a9a61a34a960cb2d
21ricardozamora <zamoraricardo21288@gmail.com>
2024-10-09 19:33:55 +0100

    se continua la tarea

--- ut2/cadenas/lost-word/main.py
+++ ut2/cadenas/lost-word/main.py
@@ -1,5 +1,8 @@
 def run(text: str, target_word: str, replace_word: str) -> str:
-    # TODO
+    word_seeked = text.find(target_word)
+    initial_text = text[:word_seeked]
+    final_text = text[word_seeked + len(target_word) :]
+    mtext = initial_text + replace_word + final_text
     return mtext
 
 
