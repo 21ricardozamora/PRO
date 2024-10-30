d87fc6952b9f81332e95b7f5a853527aa1984f36
21ricardozamora <zamoraricardo21288@gmail.com>
2024-10-30 21:46:27 +0000

    se sube todo lo hecho

---
+++ ut3/bucles/pow/args.py
@@ -0,0 +1,4 @@
+x = 3
+n = 4
+p = 1
+mult_count = 0
--- ut3/bucles/pow/main.py
+++ ut3/bucles/pow/main.py
@@ -1,5 +1,9 @@
 def run(x: int, n: int) -> int:
-    # TODO
+    mult_count = 0
+    p = 1
+    while mult_count <= n - 1:
+        p = p * x
+        mult_count += 1
     return p
 
 
