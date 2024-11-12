aff5ffbbce9baebaae2bdf290a88fa3bc22111f7
21ricardozamora <zamoraricardo21288@gmail.com>
2024-11-12 19:32:21 +0000

    se continua la tarea

--- ut3/bucles/chess-horse/args.py
+++ ut3/bucles/chess-horse/args.py
@@ -2,5 +2,5 @@ horse_x = 0
 horse_y = 0
 movements = 0
 target_possition = 0
-target_x = 5
-target_y = 4
+target_x = 6
+target_y = 5
--- ut3/bucles/chess-horse/main.py
+++ ut3/bucles/chess-horse/main.py
@@ -2,22 +2,26 @@ def run(target_x: int, target_y: int) -> int:
     horse_x = 0
     horse_y = 0
     movements = 0
-    target_possition = 0
+    incorrect_possition = -1
     for move_x in range(target_x):
+        if horse_y >= target_y:
+            movements = incorrect_possition
+            break
         horse_x += 2
         horse_y += 1
-        if horse_x - target_x == target_possition:
+        movements += 1
+        if horse_x >= target_x:
             break
         else:
             for move_y in range(target_y):
-                horse_x += 1
                 horse_y += 2
-                if (horse_y - target_y) == target_possition:
+                horse_x += 1
+                movements += 1
+                if horse_y <= target_y:
                     break
                 else:
-                    continue
-        movements += 1
-
+                    movements = incorrect_possition
+                    break
     return movements
 
 
