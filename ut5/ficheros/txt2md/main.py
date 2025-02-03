7c533abb36e53ade440cb60a2e7ea98ac2309312
21ricardozamora <zamoraricardo21288@gmail.com>
2025-02-03 01:15:02 +0000

    se continua la tarea

--- ut5/ficheros/txt2md/main.py
+++ ut5/ficheros/txt2md/main.py
@@ -1,5 +1,17 @@
 def run(input_path: str, output_path: str) -> None:
-    # TODO
+    f = open(input_path)
+    tag = '#'
+    tab = '\t'
+    output_lines = []
+    for line in f.readlines():
+        num_tabs = line.count(tab) + 1
+        line = line.strip()
+        total_hashtags = tag * num_tabs
+        output_lines.append(f'{total_hashtags} {line}')
+    f.close()
+    with open(output_path, 'w') as f:
+        for line in output_lines:
+            f.write(line + '\n')
 
 
 # DO NOT TOUCH THE CODE BELOW
