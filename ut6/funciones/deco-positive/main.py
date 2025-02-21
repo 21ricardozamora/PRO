3bd74ebe1eb2d71bfa01613ab77d9fd8994458c3
21ricardozamora <zamoraricardo21288@gmail.com>
2025-02-21 15:04:06 +0000

    se crean las tareas

---
+++ ut6/funciones/deco-positive/main.py
@@ -0,0 +1,14 @@
+# TODO
+
+
+# Adapt this function if you need to test decorator
+@assert_positive
+def run(x, y):
+    return x + y
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
