9cbf287f581f29707b5aa91c54e1608a7568d1a8
21ricardozamora <zamoraricardo21288@gmail.com>
2025-02-21 16:06:40 +0000

    se soluciona un ejercicio

---
+++ ut6/funciones/num-in-interval/args.py
@@ -0,0 +1,3 @@
+number = 5
+lower_limit = 0
+upper_limit = 1
--- ut6/funciones/num-in-interval/main.py
+++ ut6/funciones/num-in-interval/main.py
@@ -1,4 +1,8 @@
-# TODO
+def in_range(number, upper_limit, lower_limit, /):
+    valid_number = True
+    if number > lower_limit or number < upper_limit:
+        valid_number = False
+    return valid_number
 
 
 # DO NOT TOUCH THE CODE BELOW
