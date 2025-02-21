44999b22011054c17741f8322ff79b702d8fc490
21ricardozamora <zamoraricardo21288@gmail.com>
2025-02-21 16:18:54 +0000

    se continua la tarea

--- ut6/funciones/split-case/main.py
+++ ut6/funciones/split-case/main.py
@@ -1,4 +1,12 @@
-# TODO
+def split_case(words: list[str]):
+    upper_case = []
+    lower_case = []
+    for word in words:
+        if word == word.upper():
+            upper_case.append(word)
+        elif word == word.lower():
+            lower_case.append(word)
+    return upper_case, lower_case
 
 
 # DO NOT TOUCH THE CODE BELOW
