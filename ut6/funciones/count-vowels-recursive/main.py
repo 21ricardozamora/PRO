3d1f1bdf6b5d64f3eec74a5dd4852dcd1229625c
21ricardozamora <zamoraricardo21288@gmail.com>
2025-02-23 14:51:41 +0000

    se continua la tarea

--- ut6/funciones/count-vowels-recursive/main.py
+++ ut6/funciones/count-vowels-recursive/main.py
@@ -1,4 +1,10 @@
-# TODO
+def count_vowels(text: str) -> int:
+    VOWELS = 'aeiouàèìòùáéíóú'
+    num_vowels = 0
+    if len(text) == 0:
+        return 0
+    num_vowels = 1 if text.lower()[0] in VOWELS else 0
+    return num_vowels + count_vowels(text.lower()[1:])
 
 
 # DO NOT TOUCH THE CODE BELOW
