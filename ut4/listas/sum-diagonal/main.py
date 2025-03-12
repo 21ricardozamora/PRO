8147b780cf7f25ab0a10b6a5b2d2c3e79db9ce3d
21ricardozamora <zamoraricardo21288@gmail.com>
2025-03-12 19:57:05 +0000

    se continua la tarea

--- ut4/listas/sum-diagonal/main.py
+++ ut4/listas/sum-diagonal/main.py
@@ -1,5 +1,12 @@
 def run(matrix: list) -> int | None:
-    # TODO
+    size_matrix = min(len(matrix), len(matrix[0]))
+    sum_diagonal = 0
+    if size_matrix != len(matrix):
+        sum_diagonal = None
+    else:
+        for i in range(size_matrix):
+            sum_diagonal += matrix[i][i]
+
     return sum_diagonal
 
 
